3e8ce2f2ecd78b06f3ffbab8f0a4a7222794dadd
Carlos Alberto Jr <carlosalbertobzjr@gmail.com>
2026-03-10 20:28:18 -0300

    refactor: changing class and name

---
+++ ms-chats/src/main/java/dev/carlosalbertojr/mschats/MsChatsApplication.java
@@ -0,0 +1,13 @@
+package dev.carlosalbertojr.mschats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsChatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsChatsApplication.class, args);
+	}
+
+}
---
+++ ms-chats/src/test/java/dev/carlosalbertojr/mschats/MsChatsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.carlosalbertojr.mschats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsChatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
